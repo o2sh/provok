3e6e7b116484f610fa5683e9ac9820814fda6088
o2sh <ossama-hjaji@live.fr>
2021-04-29 05:11:01 +0200

    add render_state

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate failure;
 
 use clap::{crate_description, crate_name, crate_version, App, AppSettings, Arg};
 use failure::Fallible;
+use font::FontConfiguration;
 use glium::glutin::dpi::LogicalSize;
 use glium::glutin::event::Event;
 use glium::glutin::event::StartCause;
@@ -14,10 +15,10 @@ use glium::glutin::event_loop::ControlFlow;
 use glium::glutin::event_loop::EventLoop;
 use glium::glutin::window::WindowBuilder;
 use glium::glutin::ContextBuilder;
-use glium::texture::SrgbTexture2d;
-use glium::Program;
 use glium::{Display, Frame, Surface};
-use glium::{IndexBuffer, VertexBuffer};
+use input::{Input, Word};
+use render_state::RenderState;
+use std::cell::RefCell;
 use std::rc::Rc;
 use std::time::{Duration, Instant};
 
@@ -27,71 +28,10 @@ mod font;
 mod glyph_atlas;
 mod input;
 mod language;
+mod render_state;
 mod utils;
 
-use font::FontConfiguration;
-use glyph_atlas::GlyphAtlas;
-use input::{Input, Word};
-
-const PADDING: f32 = 15.;
-
-const ATLAS_SIZE: usize = 8192;
-
 static DEFAULT_INPUT_FILE: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/examples/0.json");
-
-static GLYPH_VERTEX_SHADER: &str =
-    include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/shaders/g_vertex.glsl"));
-
-static GLYPH_FRAGMENT_SHADER: &str =
-    include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/shaders/g_fragment.glsl"));
-
-static BG_VERTEX_SHADER: &str =
-    include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/shaders/bg_vertex.glsl"));
-
-static BG_FRAGMENT_SHADER: &str =
-    include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/shaders/bg_fragment.glsl"));
-
-pub const V_TOP_LEFT: usize = 0;
-pub const V_TOP_RIGHT: usize = 1;
-pub const V_BOT_LEFT: usize = 2;
-pub const V_BOT_RIGHT: usize = 3;
-
-#[derive(Copy, Clone, Default)]
-pub struct Vertex {
-    pub position: (f32, f32),
-    pub tex: (f32, f32),
-    pub fg_color: (f32, f32, f32, f32),
-    pub bg_color: (f32, f32, f32, f32),
-}
-
-implement_vertex!(Vertex, position, tex, fg_color, bg_color);
-
-pub fn compile_shaders(display: &Display) -> Fallible<(glium::Program, glium::Program)> {
-    let glyph_source = glium::program::ProgramCreationInput::SourceCode {
-        vertex_shader: GLYPH_VERTEX_SHADER,
-        fragment_shader: GLYPH_FRAGMENT_SHADER,
-        outputs_srgb: true,
-        tessellation_control_shader: None,
-        tessellation_evaluation_shader: None,
-        transform_feedback_varyings: None,
-        uses_point_size: false,
-        geometry_shader: None,
-    };
-    let glyph_program = glium::Program::new(display, glyph_source)?;
-
-    let bg_source = glium::program::ProgramCreationInput::SourceCode {
-        vertex_shader: BG_VERTEX_SHADER,
-        fragment_shader: BG_FRAGMENT_SHADER,
-        outputs_srgb: true,
-        tessellation_control_shader: None,
-        tessellation_evaluation_shader: None,
-        transform_feedback_varyings: None,
-        uses_point_size: false,
-        geometry_shader: None,
-    };
-    let bg_program = glium::Program::new(display, bg_source)?;
-    Ok((glyph_program, bg_program))
-}
 
 fn run(input_path: &str) -> Fallible<()> {
     let event_loop = EventLoop::new();
@@ -101,7 +41,7 @@ fn run(input_path: &str) -> Fallible<()> {
     let display = Display::new(wb, cb, &event_loop)?;
     let input = Rc::new(Input::new(input_path)?);
     let fontconfig = Rc::new(FontConfiguration::new(input.config.font_size, input.config.dpi)?);
-    let programs = compile_shaders(&display)?;
+    let render_state = RefCell::new(RenderState::new(&display)?);
     let mut frame_count = 0;
     let mut draw_word_count = 0;
     let mut time = 0.;
@@ -126,11 +66,11 @@ fn run(input_path: &str) -> Fallible<()> {
         *control_flow = ControlFlow::WaitUntil(next_frame_time);
         let mut target = display.draw();
 
-        paint(
+        paint_screen(
             &fontconfig,
+            &render_state,
             &display,
             &mut target,
-            &programs,
             &input.words,
             window_width,
             window_height,
@@ -145,11 +85,11 @@ fn run(input_path: &str) -> Fallible<()> {
     });
 }
 
-fn paint(
+fn paint_screen(
     fontconfig: &Rc<FontConfiguration>,
+    render_state: &RefCell<RenderState>,
     display: &Display,
     frame: &mut Frame,
-    programs: &(Program, Program),
     words: &Vec<Word>,
     window_width: f64,
     window_height: f64,
@@ -157,7 +97,7 @@ fn paint(
     time: &mut f32,
     frame_count: u32,
 ) -> Fallible<()> {
-    let (glyph_program, bg_program) = programs;
+    let mut gl_state = render_state.borrow_mut();
     let projection = euclid::Transform3D::<f32, f32, f32>::ortho(
         -(window_width as f32) / 2.0,
         window_width as f32 / 2.0,
@@ -170,234 +110,70 @@ fn paint(
 
     let draw_params =
         glium::DrawParameters { blend: glium::Blend::alpha_blending(), ..Default::default() };
-    let idx = *draw_word_count as usize % words.len();
-    draw_bg(
-        display,
-        frame,
-        bg_program,
-        &words[idx],
-        time,
-        window_width,
-        window_height,
-        &draw_params,
-        &projection,
-    )?;
-    draw_word(fontconfig, display, frame, glyph_program, &words[idx], &draw_params, &projection)?;
-    if frame_count % 30 == 0 {
-        *draw_word_count += 1;
-    }
-    Ok(())
-}
 
-fn draw_bg(
-    display: &Display,
-    frame: &mut Frame,
-    program: &Program,
-    word: &Word,
-    t: &mut f32,
-    window_width: f64,
-    window_height: f64,
-    draw_params: &glium::DrawParameters,
-    projection: &[[f32; 4]; 4],
-) -> Fallible<()> {
+    let idx = *draw_word_count as usize % words.len();
+    let word = &words[idx];
+
     frame.clear_color(
         word.canvas_color.red as f32 / 255.,
         word.canvas_color.green as f32 / 255.,
         word.canvas_color.blue as f32 / 255.,
         1.0,
     );
-    *t += 0.02;
-    if *t > 1. {
-        *t = 0.;
+
+    gl_state.compute_bg_vertices(display, window_width, window_height)?;
+
+    if frame_count % 30 == 0 {
+        gl_state.compute_glyph_vertices(word, display, fontconfig)?;
+        *draw_word_count += 1;
     }
-    let rad = 2.0 * std::f32::consts::PI * *t;
 
-    let mut verts = Vec::new();
-    let mut indices = Vec::new();
-    let (w, h) = (window_width as f32 / 2. - PADDING, window_height as f32 / 2. - PADDING);
+    *time += 0.02;
+    if *time > 1. {
+        *time = 0.;
+    }
 
-    verts.push(Vertex { position: (-w, -h), ..Default::default() });
-    verts.push(Vertex { position: (w, -h), ..Default::default() });
-    verts.push(Vertex { position: (-w, h), ..Default::default() });
-    verts.push(Vertex { position: (w, h), ..Default::default() });
-
-    indices.push(V_TOP_LEFT as u32);
-    indices.push(V_TOP_RIGHT as u32);
-    indices.push(V_BOT_LEFT as u32);
-
-    indices.push(V_TOP_RIGHT as u32);
-    indices.push(V_BOT_LEFT as u32);
-    indices.push(V_BOT_RIGHT as u32);
-
-    let vertex_buffer = VertexBuffer::dynamic(display, &verts)?;
-    let index_buffer =
-        IndexBuffer::new(display, glium::index::PrimitiveType::TrianglesList, &indices)?;
+    let rad = 2.0 * std::f32::consts::PI * *time;
 
     frame.draw(
-        &vertex_buffer,
-        &index_buffer,
-        &program,
+        gl_state.bg_vertex_buffer.as_ref().unwrap(),
+        gl_state.bg_index_buffer.as_ref().unwrap(),
+        &gl_state.bg_program,
         &uniform! {
-            projection: *projection,
+            projection: projection,
             rad: rad
         },
-        draw_params,
+        &draw_params,
     )?;
-    Ok(())
-}
 
-fn draw_word(
-    fontconfig: &Rc<FontConfiguration>,
-    display: &Display,
-    frame: &mut Frame,
-    program: &Program,
-    word: &Word,
-    draw_params: &glium::DrawParameters,
-    projection: &[[f32; 4]; 4],
-) -> Fallible<()> {
-    let mut glyph_atlas = GlyphAtlas::new(display, ATLAS_SIZE)?;
-
-    let (mut glyph_vertex_buffer, glyph_index_buffer) =
-        compute_glyph_vertices(word, display, &mut glyph_atlas, fontconfig)?;
-
-    if let Some(bg_color) = word.style.bg_color {
-        let (bg_glyph_vertex_buffer, bg_glyph_index_buffer) =
-            compute_bg_glyph_vertices(bg_color, &mut glyph_vertex_buffer, display)?;
+    if gl_state.draw_bg {
         frame.draw(
-            &bg_glyph_vertex_buffer,
-            &bg_glyph_index_buffer,
-            &program,
+            gl_state.glyph_bg_vertex_buffer.as_ref().unwrap(),
+            gl_state.glyph_bg_index_buffer.as_ref().unwrap(),
+            &gl_state.glyph_program,
             &uniform! {
-                projection: *projection,
+                projection: projection,
                 draw_bg: true
             },
-            draw_params,
+            &draw_params,
         )?;
     }
 
-    let tex = glyph_atlas.atlas.texture();
+    let tex = gl_state.glyph_atlas.atlas.texture();
 
     frame.draw(
-        &glyph_vertex_buffer,
-        &glyph_index_buffer,
-        &program,
+        gl_state.glyph_vertex_buffer.as_ref().unwrap(),
+        gl_state.glyph_index_buffer.as_ref().unwrap(),
+        &gl_state.glyph_program,
         &uniform! {
-            projection: *projection,
+            projection: projection,
             glyph_tex: &*tex,
             draw_bg: false
         },
-        draw_params,
+        &draw_params,
     )?;
 
     Ok(())
-}
-
-fn compute_bg_glyph_vertices(
-    bg_color: color::RgbColor,
-    glyph_vertex_buffer: &mut VertexBuffer<Vertex>,
-    display: &Display,
-) -> Fallible<(VertexBuffer<Vertex>, IndexBuffer<u32>)> {
-    let bg_color = color::to_tuple_rgba(bg_color);
-    let mut verts = Vec::new();
-    let mut indices = Vec::new();
-    let (mut top, mut left, mut bottom, mut right) = (0f32, 0f32, 0f32, 0f32);
-    let g_verts = glyph_vertex_buffer.slice_mut(..).unwrap().map_read();
-    for v in g_verts.iter() {
-        left = left.min(v.position.0);
-        right = right.max(v.position.0);
-        top = top.min(v.position.1);
-        bottom = bottom.max(v.position.1);
-    }
-
-    left -= PADDING;
-    right += PADDING;
-    top -= PADDING;
-    bottom += PADDING;
-
-    verts.push(Vertex { position: (left, top), bg_color, ..Default::default() });
-    verts.push(Vertex { position: (right, top), bg_color, ..Default::default() });
-    verts.push(Vertex { position: (left, bottom), bg_color, ..Default::default() });
-    verts.push(Vertex { position: (right, bottom), bg_color, ..Default::default() });
-
-    indices.push(V_TOP_LEFT as u32);
-    indices.push(V_TOP_RIGHT as u32);
-    indices.push(V_BOT_LEFT as u32);
-
-    indices.push(V_TOP_RIGHT as u32);
-    indices.push(V_BOT_LEFT as u32);
-    indices.push(V_BOT_RIGHT as u32);
-
-    Ok((
-        VertexBuffer::dynamic(display, &verts)?,
-        IndexBuffer::new(display, glium::index::PrimitiveType::TrianglesList, &indices)?,
-    ))
-}
-fn compute_glyph_vertices(
-    word: &Word,
-    display: &Display,
-    glyph_atlas: &mut GlyphAtlas<SrgbTexture2d>,
-    fontconfig: &FontConfiguration,
-) -> Fallible<(VertexBuffer<Vertex>, IndexBuffer<u32>)> {
-    let mut verts = Vec::new();
-    let mut indices = Vec::new();
-    let fg_color = color::to_tuple_rgba(word.style.fg_color);
-
-    let font = fontconfig.get_font(&word.style)?;
-    let glyph_infos = font.shape(&word.text)?;
-    let width = glyph_infos.iter().fold(0., |acc, info| acc + info.x_advance.get() as f32);
-    let mut x = -width / 2.;
-    let mut y = 0.;
-    for glyph_info in &glyph_infos {
-        let rasterized_glyph = font.rasterize(glyph_info.glyph_pos)?;
-        let glyph = glyph_atlas.load_glyph(rasterized_glyph, &glyph_info)?;
-
-        let x0 = x + (glyph.x_offset + glyph.bearing_x).get() as f32;
-        let y0 = y - (glyph.y_offset + glyph.bearing_y).get() as f32;
-
-        let x1 = x0 + glyph.texture.width as f32;
-        let y1 = y0 + glyph.texture.height as f32;
-
-        x += glyph_info.x_advance.get() as f32;
-        y += glyph_info.y_advance.get() as f32;
-        let idx = verts.len() as u32;
-        verts.push(Vertex {
-            position: (x0, y0),
-            tex: (glyph.texture.tex_coords.min_x(), glyph.texture.tex_coords.min_y()),
-            fg_color,
-            ..Default::default()
-        });
-        verts.push(Vertex {
-            position: (x1, y0),
-            tex: (glyph.texture.tex_coords.max_x(), glyph.texture.tex_coords.min_y()),
-            fg_color,
-            ..Default::default()
-        });
-        verts.push(Vertex {
-            position: (x0, y1),
-            tex: (glyph.texture.tex_coords.min_x(), glyph.texture.tex_coords.max_y()),
-            fg_color,
-            ..Default::default()
-        });
-        verts.push(Vertex {
-            position: (x1, y1),
-            tex: (glyph.texture.tex_coords.max_x(), glyph.texture.tex_coords.max_y()),
-            fg_color,
-            ..Default::default()
-        });
-
-        indices.push(idx + V_TOP_LEFT as u32);
-        indices.push(idx + V_TOP_RIGHT as u32);
-        indices.push(idx + V_BOT_LEFT as u32);
-
-        indices.push(idx + V_TOP_RIGHT as u32);
-        indices.push(idx + V_BOT_LEFT as u32);
-        indices.push(idx + V_BOT_RIGHT as u32);
-    }
-
-    Ok((
-        VertexBuffer::dynamic(display, &verts)?,
-        IndexBuffer::new(display, glium::index::PrimitiveType::TrianglesList, &indices)?,
-    ))
 }
 
 fn main() -> Fallible<()> {
---
+++ src/render_state.rs
@@ -0,0 +1,256 @@
+use crate::color;
+use crate::font::FontConfiguration;
+use crate::glyph_atlas::GlyphAtlas;
+use crate::input::Word;
+use failure::Fallible;
+use glium::texture::SrgbTexture2d;
+use glium::Display;
+use glium::Program;
+use glium::{IndexBuffer, VertexBuffer};
+
+const PADDING: f32 = 15.;
+
+const ATLAS_SIZE: usize = 8192;
+
+static GLYPH_VERTEX_SHADER: &str =
+    include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/shaders/g_vertex.glsl"));
+
+static GLYPH_FRAGMENT_SHADER: &str =
+    include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/shaders/g_fragment.glsl"));
+
+static BG_VERTEX_SHADER: &str =
+    include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/shaders/bg_vertex.glsl"));
+
+static BG_FRAGMENT_SHADER: &str =
+    include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/shaders/bg_fragment.glsl"));
+
+pub const V_TOP_LEFT: usize = 0;
+pub const V_TOP_RIGHT: usize = 1;
+pub const V_BOT_LEFT: usize = 2;
+pub const V_BOT_RIGHT: usize = 3;
+
+#[derive(Copy, Clone, Default)]
+pub struct Vertex {
+    pub position: (f32, f32),
+    pub tex: (f32, f32),
+    pub fg_color: (f32, f32, f32, f32),
+    pub bg_color: (f32, f32, f32, f32),
+}
+
+implement_vertex!(Vertex, position, tex, fg_color, bg_color);
+
+pub struct RenderState {
+    pub glyph_atlas: GlyphAtlas<SrgbTexture2d>,
+    pub bg_program: Program,
+    pub glyph_program: Program,
+    pub glyph_vertex_buffer: Option<VertexBuffer<Vertex>>,
+    pub glyph_index_buffer: Option<IndexBuffer<u32>>,
+    pub glyph_bg_vertex_buffer: Option<VertexBuffer<Vertex>>,
+    pub glyph_bg_index_buffer: Option<IndexBuffer<u32>>,
+    pub bg_vertex_buffer: Option<VertexBuffer<Vertex>>,
+    pub bg_index_buffer: Option<IndexBuffer<u32>>,
+    pub draw_bg: bool,
+}
+
+impl RenderState {
+    pub fn new(display: &Display) -> Fallible<Self> {
+        let (glyph_program, bg_program) = compile_shaders(display)?;
+        let glyph_atlas = GlyphAtlas::new(display, ATLAS_SIZE)?;
+        Ok(Self {
+            glyph_atlas,
+            bg_program,
+            glyph_program,
+            glyph_vertex_buffer: None,
+            glyph_index_buffer: None,
+            glyph_bg_vertex_buffer: None,
+            glyph_bg_index_buffer: None,
+            bg_vertex_buffer: None,
+            bg_index_buffer: None,
+            draw_bg: false,
+        })
+    }
+
+    pub fn compute_glyph_vertices(
+        &mut self,
+        word: &Word,
+        display: &Display,
+        fontconfig: &FontConfiguration,
+    ) -> Fallible<()> {
+        self.compute_g_vertices(word, display, fontconfig)?;
+
+        if let Some(bg_color) = word.style.bg_color {
+            self.draw_bg = true;
+            self.compute_bg_g_vertices(bg_color, display)?;
+        } else {
+            self.draw_bg = false;
+        }
+        Ok(())
+    }
+
+    pub fn compute_g_vertices(
+        &mut self,
+        word: &Word,
+        display: &Display,
+        fontconfig: &FontConfiguration,
+    ) -> Fallible<()> {
+        let mut verts = Vec::new();
+        let mut indices = Vec::new();
+        let fg_color = color::to_tuple_rgba(word.style.fg_color);
+
+        let font = fontconfig.get_font(&word.style)?;
+        let glyph_infos = font.shape(&word.text)?;
+        let width = glyph_infos.iter().fold(0., |acc, info| acc + info.x_advance.get() as f32);
+        let mut x = -width / 2.;
+        let mut y = 0.;
+        for glyph_info in &glyph_infos {
+            let rasterized_glyph = font.rasterize(glyph_info.glyph_pos)?;
+            let glyph = self.glyph_atlas.load_glyph(rasterized_glyph, &glyph_info)?;
+
+            let x0 = x + (glyph.x_offset + glyph.bearing_x).get() as f32;
+            let y0 = y - (glyph.y_offset + glyph.bearing_y).get() as f32;
+
+            let x1 = x0 + glyph.texture.width as f32;
+            let y1 = y0 + glyph.texture.height as f32;
+
+            x += glyph_info.x_advance.get() as f32;
+            y += glyph_info.y_advance.get() as f32;
+            let idx = verts.len() as u32;
+            verts.push(Vertex {
+                position: (x0, y0),
+                tex: (glyph.texture.tex_coords.min_x(), glyph.texture.tex_coords.min_y()),
+                fg_color,
+                ..Default::default()
+            });
+            verts.push(Vertex {
+                position: (x1, y0),
+                tex: (glyph.texture.tex_coords.max_x(), glyph.texture.tex_coords.min_y()),
+                fg_color,
+                ..Default::default()
+            });
+            verts.push(Vertex {
+                position: (x0, y1),
+                tex: (glyph.texture.tex_coords.min_x(), glyph.texture.tex_coords.max_y()),
+                fg_color,
+                ..Default::default()
+            });
+            verts.push(Vertex {
+                position: (x1, y1),
+                tex: (glyph.texture.tex_coords.max_x(), glyph.texture.tex_coords.max_y()),
+                fg_color,
+                ..Default::default()
+            });
+
+            indices.push(idx + V_TOP_LEFT as u32);
+            indices.push(idx + V_TOP_RIGHT as u32);
+            indices.push(idx + V_BOT_LEFT as u32);
+
+            indices.push(idx + V_TOP_RIGHT as u32);
+            indices.push(idx + V_BOT_LEFT as u32);
+            indices.push(idx + V_BOT_RIGHT as u32);
+        }
+
+        self.glyph_vertex_buffer = Some(VertexBuffer::dynamic(display, &verts)?);
+        self.glyph_index_buffer =
+            Some(IndexBuffer::new(display, glium::index::PrimitiveType::TrianglesList, &indices)?);
+        Ok(())
+    }
+
+    pub fn compute_bg_g_vertices(
+        &mut self,
+        bg_color: color::RgbColor,
+        display: &Display,
+    ) -> Fallible<()> {
+        let bg_color = color::to_tuple_rgba(bg_color);
+        let mut verts = Vec::new();
+        let mut indices = Vec::new();
+        let (mut top, mut left, mut bottom, mut right) = (0f32, 0f32, 0f32, 0f32);
+        let glyph_vertex_buffer = self.glyph_vertex_buffer.as_mut().unwrap();
+        let g_verts = glyph_vertex_buffer.slice_mut(..).unwrap().map_read();
+        for v in g_verts.iter() {
+            left = left.min(v.position.0);
+            right = right.max(v.position.0);
+            top = top.min(v.position.1);
+            bottom = bottom.max(v.position.1);
+        }
+
+        left -= PADDING;
+        right += PADDING;
+        top -= PADDING;
+        bottom += PADDING;
+
+        verts.push(Vertex { position: (left, top), bg_color, ..Default::default() });
+        verts.push(Vertex { position: (right, top), bg_color, ..Default::default() });
+        verts.push(Vertex { position: (left, bottom), bg_color, ..Default::default() });
+        verts.push(Vertex { position: (right, bottom), bg_color, ..Default::default() });
+
+        indices.push(V_TOP_LEFT as u32);
+        indices.push(V_TOP_RIGHT as u32);
+        indices.push(V_BOT_LEFT as u32);
+
+        indices.push(V_TOP_RIGHT as u32);
+        indices.push(V_BOT_LEFT as u32);
+        indices.push(V_BOT_RIGHT as u32);
+
+        self.glyph_bg_vertex_buffer = Some(VertexBuffer::dynamic(display, &verts)?);
+        self.glyph_bg_index_buffer =
+            Some(IndexBuffer::new(display, glium::index::PrimitiveType::TrianglesList, &indices)?);
+        Ok(())
+    }
+
+    pub fn compute_bg_vertices(
+        &mut self,
+        display: &Display,
+        window_width: f64,
+        window_height: f64,
+    ) -> Fallible<()> {
+        let mut verts = Vec::new();
+        let mut indices = Vec::new();
+        let (w, h) = (window_width as f32 / 2. - PADDING, window_height as f32 / 2. - PADDING);
+
+        verts.push(Vertex { position: (-w, -h), ..Default::default() });
+        verts.push(Vertex { position: (w, -h), ..Default::default() });
+        verts.push(Vertex { position: (-w, h), ..Default::default() });
+        verts.push(Vertex { position: (w, h), ..Default::default() });
+
+        indices.push(V_TOP_LEFT as u32);
+        indices.push(V_TOP_RIGHT as u32);
+        indices.push(V_BOT_LEFT as u32);
+
+        indices.push(V_TOP_RIGHT as u32);
+        indices.push(V_BOT_LEFT as u32);
+        indices.push(V_BOT_RIGHT as u32);
+
+        self.bg_vertex_buffer = Some(VertexBuffer::dynamic(display, &verts)?);
+        self.bg_index_buffer =
+            Some(IndexBuffer::new(display, glium::index::PrimitiveType::TrianglesList, &indices)?);
+
+        Ok(())
+    }
+}
+
+fn compile_shaders(display: &Display) -> Fallible<(glium::Program, glium::Program)> {
+    let glyph_source = glium::program::ProgramCreationInput::SourceCode {
+        vertex_shader: GLYPH_VERTEX_SHADER,
+        fragment_shader: GLYPH_FRAGMENT_SHADER,
+        outputs_srgb: true,
+        tessellation_control_shader: None,
+        tessellation_evaluation_shader: None,
+        transform_feedback_varyings: None,
+        uses_point_size: false,
+        geometry_shader: None,
+    };
+    let glyph_program = glium::Program::new(display, glyph_source)?;
+
+    let bg_source = glium::program::ProgramCreationInput::SourceCode {
+        vertex_shader: BG_VERTEX_SHADER,
+        fragment_shader: BG_FRAGMENT_SHADER,
+        outputs_srgb: true,
+        tessellation_control_shader: None,
+        tessellation_evaluation_shader: None,
+        transform_feedback_varyings: None,
+        uses_point_size: false,
+        geometry_shader: None,
+    };
+    let bg_program = glium::Program::new(display, bg_source)?;
+    Ok((glyph_program, bg_program))
+}
